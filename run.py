f2623e0912a7eecde74e78c3df4d34ab6290fdc6
BUAAkang <932563835@qq.com>
2023-08-24 20:16:47 +0800

    add a script to simplify operation about git

--- run.py
+++ run.py
@@ -15,6 +15,6 @@ if platform.system() != "Linux":
   os.system("python manage.py runserver 8001")
 # 本地环境，直接运行
 else:
-  os.system("python manage.py runserver 0.0.0.0:8001 > log.txt & \n")
+  os.system("python manage.py runserver 0.0.0.0:8001 & \n")
 print("The backend is running!")
 # 服务器环境，后台运行
