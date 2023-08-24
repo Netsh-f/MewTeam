ae2ba6702263af736dd538f16745d4a8db09fe22
elaikona <zwj1813@163.com>
2023-08-24 13:52:53 +0800

    init user and start register

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    uid = models.BigAutoField(primary_key=True)
+    email = models.EmailField(max_length=63)
+    username = models.CharField(max_length=63)
+    password = models.CharField(max_length=71)
