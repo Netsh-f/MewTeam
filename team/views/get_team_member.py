01b265aeca1f1e63db268cdfb0be59cf700a2ce6
elaikona <zwj1813@163.com>
2023-08-24 20:25:08 +0800

    get team members

--- team/views/get_team_member.py
+++ team/views/get_team_member.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from shared.error import Error
 from shared.res_temp import ResponseTemplate
 from shared.token import get_identity_from_token, verify_token
+from team.models import Team, UserTeamShip
+from user.models import User
 
 
 @api_view(['POST'])
@@ -11,3 +13,10 @@ def get_team_member(request):
         user_id = get_identity_from_token(user_token)
     else:
         return ResponseTemplate(Error.TOKEN_INVALID, "token is invalid")
+    data = request.data
+    team_id = data['team_id']
+    user_team_ships = UserTeamShip.objects.filter(team_id=team_id)
+    team_members = [user_team_ship.user for user_team_ship in user_team_ships]
+    team_members_info = []
+    for user in team_members:
+        pass
