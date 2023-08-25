bfd3d4a47e7b2fd97d3cc1a665bf309f6120a77c
BUAAkang <932563835@qq.com>
2023-08-26 00:38:54 +0800

    sth about file manage

--- MewTeam/settings.py
+++ MewTeam/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 import secrets
 import string
 from pathlib import Path
@@ -33,6 +34,10 @@ with open("secrets.yaml", 'r') as f:
 with open("config.yaml", 'r') as f:
     CONFIG = yaml.safe_load(f)
 ERROR_CODE = CONFIG['ERROR_CODE']
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
