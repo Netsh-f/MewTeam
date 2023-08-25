28868326186e7b99c18702385a619e0451d999ba
BUAAkang <932563835@qq.com>
2023-08-25 11:27:13 +0800

    optimize some interfaces about user modules

--- user/views/info.py
+++ user/views/info.py
@@ -13,18 +13,33 @@ from user.models import User
 
 @api_view(['GET'])
 def infoShow(request, user_id):
-    user = User.objects.get(id=user_id)
-    if user:
-        return ResponseTemplate(Error.SUCCESS, '用户信息返回成功！', data=user)
+    user = User.objects.filter(id=user_id).first()
+    if user != None:
+        data = {
+            'name': user.name,
+            'email': user.email,
+            'nickname': user.nickname,
+        }
+        return ResponseTemplate(Error.SUCCESS, '用户信息返回成功！', data=data)
     else:
         return ResponseTemplate(Error.USER_NOT_EXISTS, '用户不存在')
 
-@api_view(['POST'])
+@api_view(['PUT'])
 def infoEdit(request, user_id):
-    data = request.data
-    # user = User.objects.get(id=user_id)
-    # if user:
-    #     if data.get('name'):
-    # #     return ResponseTemplate(Error.SUCCESS, '用户信息返回成功！', data=user)
-    # else:
-    #     return ResponseTemplate(Error.USER_NOT_EXISTS, '用户不存在')
+    data:dict = request.data
+    try:
+        user = User.objects.get(id=user_id)
+        name = data.get('name')
+        email = data.get('email')
+        nickname = data.get('nickname')
+
+        if name != None:
+            user.name = name
+        if email != None:
+            user.email = email
+        if nickname != None:
+            user.nickname = nickname
+        user.save()
+        return ResponseTemplate(Error.SUCCESS, '用户信息修改成功！')
+    except Exception:
+        return ResponseTemplate(Error.USER_NOT_EXISTS, '用户不存在')
