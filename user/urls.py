daea9504cc7d6de2b60d827d9d3625fbd1b38307
BUAAkang <932563835@qq.com>
2023-08-25 14:18:03 +0800

    sync url template

--- user/urls.py
+++ user/urls.py
@@ -9,10 +9,10 @@ from django.urls import path
 from user.views import register, login, logout, info
 
 urlpatterns = [
-    path('register', register.register, name='register'),
-    path('login', login.login, name='login'),
-    path('logout', logout.logout, name='logout'),
-    path('logoff/<int:user_id>', logout.logoff, name='writeoff'),
-    path('info/show/<int:user_id>', info.infoShow, name='infoShow'),
-    path('info/edit/<int:user_id>', info.infoEdit, name='infoEdit')
+    path('register/', register.register, name='register'),
+    path('login/', login.login, name='login'),
+    path('logout/', logout.logout, name='logout'),
+    path('logoff/<int:user_id>/', logout.logoff, name='writeoff'),
+    path('info/show/<int:user_id>/', info.infoShow, name='infoShow'),
+    path('info/edit/<int:user_id>/', info.infoEdit, name='infoEdit')
 ]
