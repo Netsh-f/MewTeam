d877fea2ffecf96a101722e4ad5126a27de1f119
BUAAkang <932563835@qq.com>
2023-08-26 20:52:01 +0800

    add urls in team

--- team/urls.py
+++ team/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('teams/join/', invitation.join_team_with_invitation),
     path('teams/<int:team_id>', teams.disband_team),
     path('teams/<int:team_id>/users/<int:user_id>/', members.remove_team_user),
-    path('teams/<int:team_id>/users/<int:user_id>/join', invitation.join_team)
+    path('teams/<int:team_id>/users/<int:user_id>/join/', invitation.join_team)
 ]
