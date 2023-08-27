223432c7ce848b9533d17acd0a7a86da053c5527
BUAAkang <932563835@qq.com>
2023-08-27 09:25:02 +0800

    rm file_test.py

--- file_test.py
+++
@@ -1,9 +0,0 @@
-# ------- Litang Save The World! -------
-#
-# @Time    : 2023/8/26 11:26
-# @Author  : Lynx
-# @File    : file_test.py
-#
-import os.path
-
-print(os.path.exists("/media/avatar/5.jpg"))
