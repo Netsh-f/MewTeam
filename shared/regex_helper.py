d684bda55ab7035c94e9b6c8267cba5b22499930
elaikona <zwj1813@163.com>
2023-09-01 13:34:43 +0800

    update regex mention get

---
+++ shared/regex_helper.py
@@ -0,0 +1,15 @@
+"""
+============================
+# @Time    : 2023/9/1 13:11
+# @Author  : Elaikona
+# @FileName: regex_helper.py
+===========================
+"""
+import re
+
+
+def extract_user_ids(chat_content):
+    pattern = r'<usertag>(\d+)<usertag>'
+    matches = re.findall(pattern, chat_content)
+    user_ids = [int(match) for match in matches]
+    return user_ids
